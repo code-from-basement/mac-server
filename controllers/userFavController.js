30c35a47f3a4feb736c6b873b49d525c83b75eb4
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-06 16:37:48 +0200

    add logic to handle the click event on the menu favorite like button add logic to handle the click event on the menu favorite like button in database and delete the liked item from the favorite list in database

--- controllers/userFavController.js
+++ controllers/userFavController.js
@@ -9,31 +9,52 @@ exports.newFavItem = async (req, res) => {
 
   const allUsername = await User.find();
   const findUsername = await User.findOne({ username: username });
-  console.log(username, favoriteList, findUsername);
   try {
     const newUserFav = new User({
       username,
       favoriteList,
     });
+
     if (!findUsername) {
       await newUserFav.save();
       res.status(201).json({
         status: "new user has been created in database!",
         data: { username: newUserFav.username, favoriteList: newUserFav.favoriteList },
       });
-    } else {
-      res.status(200).json({
-        status: "success",
-        data: { username: findUsername.username, favoriteList: findUsername.favoriteList },
-      });
-
-      // username.favoriteList.push(...favoriteList);
-      // await username.save();
-      // res.status(201).json({
-      //   success: "success",
-      //   data: [...new Set(username.favoriteList)],
-      // });
     }
+
+    if (findUsername && favoriteList.length > 0) {
+      const searchLikedItem = findUsername.favoriteList.filter((item) => item === favoriteList[0]);
+      console.log(searchLikedItem);
+
+      // remove like item when the item is already in the list
+      if (findUsername.favoriteList.length > 0 && searchLikedItem) {
+        console.log("findUsername.favoriteList.length > 0");
+        findUsername.favoriteList = findUsername.favoriteList.filter((item) => item !== searchLikedItem[0]);
+        await findUsername.save();
+        return res.status(200).json({ message: "item removed from favorite list!", data: { favoriteList } });
+      }
+
+      // add new like item when user has no favorite in the list
+      if (findUsername.favoriteList.length === 0) {
+        console.log("findUsername.favoriteList.length === 0");
+        findUsername.favoriteList.push(...favoriteList);
+        await findUsername.save();
+        return res.status(200).json({ message: "item added to favorite list!", data: { favoriteList } });
+      }
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: { username: findUsername.username, favoriteList: findUsername.favoriteList },
+    });
+
+    // username.favoriteList.push(...favoriteList);
+    // await username.save();
+    // res.status(201).json({
+    //   success: "success",
+    //   data: [...new Set(username.favoriteList)],
+    // });
   } catch (err) {
     res.status(403).json({ status: "failedx", message: err.message });
   }
