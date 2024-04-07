964570805c15eed7b3719ad4eda4ef59962d7d35
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-07 12:52:12 +0200

    done with item like button in backend and frontend

--- controllers/userFavController.js
+++ controllers/userFavController.js
@@ -7,54 +7,60 @@ const User = require("../models/usersFav");
 exports.newFavItem = async (req, res) => {
   const { username, favoriteList } = req.body;
 
+  // all data in diabase
   const allUsername = await User.find();
+  // find user in database
   const findUsername = await User.findOne({ username: username });
   try {
+    //
     const newUserFav = new User({
       username,
       favoriteList,
     });
 
-    if (!findUsername) {
+    // if user is not in the database
+    if (!findUsername && favoriteList.length === 0) {
       await newUserFav.save();
-      res.status(201).json({
+      return res.status(201).json({
         status: "new user has been created in database!",
         data: { username: newUserFav.username, favoriteList: newUserFav.favoriteList },
       });
     }
-
+    // if user is already in the database
+    if (findUsername && favoriteList.length === 0) {
+      return res.status(200).json({
+        message: "here is favorite list of the user!",
+        data: findUsername.favoriteList,
+      });
+    }
+    // Logic behind when user LIKE an ITEM
     if (findUsername && favoriteList.length > 0) {
-      const searchLikedItem = findUsername.favoriteList.filter((item) => item === favoriteList[0]);
-      console.log(searchLikedItem);
+      const searchLikedItem = findUsername.favoriteList.filter((item) => item == favoriteList[0]).length > 0 ? true : false;
+      console.log(searchLikedItem, findUsername.favoriteList, favoriteList[0]);
 
-      // remove like item when the item is already in the list
+      // REMOVE item when the item is already in the list
       if (findUsername.favoriteList.length > 0 && searchLikedItem) {
-        console.log("findUsername.favoriteList.length > 0");
-        findUsername.favoriteList = findUsername.favoriteList.filter((item) => item !== searchLikedItem[0]);
+        console.log("remove item - existed item !");
+        findUsername.favoriteList = findUsername.favoriteList.filter((item) => item !== favoriteList[0]);
         await findUsername.save();
-        return res.status(200).json({ message: "item removed from favorite list!", data: { favoriteList } });
+        return res.status(200).json({ message: "item removed from favorite list!", data: findUsername.favoriteList });
       }
-
-      // add new like item when user has no favorite in the list
-      if (findUsername.favoriteList.length === 0) {
-        console.log("findUsername.favoriteList.length === 0");
+      // ADD new item when user HAS Item favorite in the list
+      if (findUsername.favoriteList.length > 0 && !searchLikedItem) {
+        console.log("add new item - when favorite list is NOT empty!!!");
         findUsername.favoriteList.push(...favoriteList);
         await findUsername.save();
-        return res.status(200).json({ message: "item added to favorite list!", data: { favoriteList } });
+        return res.status(200).json({ message: "New item has been added to favorite list!", data: findUsername.favoriteList });
+      }
+
+      // ADD new item when user HAS NO item in the favorite list
+      if (findUsername.favoriteList.length === 0) {
+        console.log("add new item - when favorite list IS empty!");
+        findUsername.favoriteList.push(...favoriteList);
+        await findUsername.save();
+        return res.status(200).json({ message: "item added to favorite list!", data: findUsername.favoriteList });
       }
     }
-
-    res.status(200).json({
-      status: "success",
-      data: { username: findUsername.username, favoriteList: findUsername.favoriteList },
-    });
-
-    // username.favoriteList.push(...favoriteList);
-    // await username.save();
-    // res.status(201).json({
-    //   success: "success",
-    //   data: [...new Set(username.favoriteList)],
-    // });
   } catch (err) {
     res.status(403).json({ status: "failedx", message: err.message });
   }
